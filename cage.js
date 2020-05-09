7cbd823c27f7e6d637eda8a2c6bfb5ea90838d64
unknown <teemu.eerola@tuni.fi>
2020-05-09 15:35:22 +0300

    yritys

--- cage.js
+++ cage.js
@@ -1,15 +1,46 @@
 // reading data from ruuvi
 const ruuvi = require('node-ruuvitag');
+let temps = [];
+let pressures=[];
+let humiditis=[];
+let save=51;
 
 ruuvi.on('found', tag => {
   console.log('Found RuuviTag, id: ' + tag.id);
   tag.on('updated', data => {
-    console.log('Got data from RuuviTag ' + tag.id + ':\n' +
-      JSON.stringify(data, null, '\t'));
+    //console.log('Tyotaa tallasta dataa ' + tag.id + ':\n' +
+    //  JSON.stringify(data, null, '\t'));
+	//console.log(' temp: '+data.temperature+' pres: '+data.pressure+' hud: '+data.humidity+ '\n');
+	if (save>50)
+		temps.push(data.temperature);
+		pressures.push(data.pressure);
+		humiditis.push(data.humidity);
+		save=0;
+		if (temps.lenght > 24*60)
+			temps.shift();
+			pressures.shift();
+			humiditis.shift();
+	else
+		save += 1;
   });
 });
+console.log('temps '+temps+'\npres '+pressures+'\nhumids '+humiditis);
 
 // handeling the data (current, hour avg, day avg)
 
 // telebot commands
-bot.on('/hello', (msg) => msg.reply.text("Hello! :)"));
+const TeleBot = require('telebot');
+const bot = new TeleBot({
+  token: 1087700060:AAGYqsnxRaxEAQ6hTCMm4o_Sj4sjcQaSDGQ,
+  polling: {
+    interval: 1000, // How often check updates (in ms).
+    timeout: 0, // Update polling timeout (0 - short polling).
+    limit: 100, // Limits the number of updates to be retrieved.
+    retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
+  }
+});
+// wait for '/hello' command
+bot.on('/hello', (msg) => msg.reply.text("terve vuoan!"));
+bot.on('/maxt',(msg)=>msg.reply.text(temps[1]));
+// start bot
+bot.start();
