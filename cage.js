5bba596fcaed32cd2dbfde9681ed46770c5e5323
teerol <65023286+teerol@users.noreply.github.com>
2021-01-12 19:03:38 +0200

    Update cage.js

--- cage.js
+++ cage.js
@@ -120,12 +120,12 @@ function sarvet() {
 
 const TeleBot = require('telebot');
 const bot = new TeleBot({
-  token: '1087700060:AAEysZAIc6S7CQAQhp_BoSuE9yEuNiN4Gj0',
+  token: '***',
   polling: {
-    interval: 1000, // How often check updates (in ms).
-    timeout: 0, // Update polling timeout (0 - short polling).
-    limit: 100, // Limits the number of updates to be retrieved.
-    retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
+    interval: 1000, 
+    timeout: 0,
+    limit: 100,
+    retryTimeout: 5000, 
   }
 });
 // wait for command
